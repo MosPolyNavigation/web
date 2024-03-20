85f072df451a31388092a4bac1df5711a4d93195
kaler654 <kaler654@mail.ru>
2024-03-20 11:24:00 +0300

    full size input in searchBar

--- src/components/searchBar/SearchBar.jsx
+++ src/components/searchBar/SearchBar.jsx
@@ -6,11 +6,11 @@ import searchBarLightIcon from "../../images/searchBarLightIcon.svg";
 const SearchBar = ({placeholder, props, searchQuery, isShowSearch}) => {
     return (
         <div className="searchBar_packet">
-            <input className="openKeyboard" inputMode="text" disbabled/>
+            {/*<input className="openKeyboard" inputMode="text" disbabled/>*/}
             <img src={isShowSearch ? searchBarLightIcon : searchBarDarkIcon} alt="search Icon"/>
             {/*<span className="searchPsevdoInput_span">Поиск...</span>*/}
             <input value={searchQuery} onChange={props} type="text" className="searchBar_input"
-                   placeholder={placeholder}/>
+                   placeholder={placeholder} inputMode="text"/>
         </div>
     );
 };
